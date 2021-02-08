ad4feea471ec2f50ce2ef6740a4107ab37272ed0
tetianaziatikova <74005676+tetianaziatikova@users.noreply.github.com>
2021-02-08 19:11:38 +0200

    дописала задание №6

--- js/06-task.js
+++ js/06-task.js
@@ -1,14 +1,39 @@
 let inputRef = document.querySelector('#validation-input');
-let inputLenght = inputRef.getAttribute('data-length');
+let inputLenght = inputRef.dataset.length;
+// console.log(inputLenght);
+
+inputRef.addEventListener('blur', onInputBlur)
+inputRef.addEventListener('input', inputValueHandler)
+
+function onInputBlur({target: {value}}) {
+    value.length === 6 ? inputRef.classList.add('valid') : inputRef.classList.add('invalid')
+}
+
+function inputValueHandler() {
+    inputRef.classList.remove('valid', 'invalid')
+}
+
+// function onInputBlur(event) {
+//     console.log(event.target.value.length);
+//     let length = event.target.value.length
+//     length === 6 ? inputRef.classList.add('valid') : inputRef.classList.add('invalid')
+// }
 
 
-inputRef.addEventListener('focus', () => {
-    if (inputLenght === '6') {
-        inputRef.classList.remove('invalid');
-        inputRef.classList.add('valid');
-    }
-    if (inputLenght !== '6') {
-        inputRef.classList.add('invalid');
-    }
-});
+
+// inputRef.addEventListener('blur', () => {
+//     if (inputLenght === '6') {
+//         inputRef.classList.remove('invalid');
+//         inputRef.classList.add('valid');
+//     } else if (inputLenght > '6') {
+//         console.log('invalid');
+
+//         inputRef.classList.add('invalid')
+//     };
+
+    
+// });
+
+
+
 
